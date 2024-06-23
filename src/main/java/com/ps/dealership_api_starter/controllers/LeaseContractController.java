7462dcabdb5751806b2d321a39dc62d05b049b67
hibbaafzal <166542360+hibbaafzal@users.noreply.github.com>
2024-06-23 18:13:27 -0400

    implemented methods in lease controller class

--- src/main/java/com/ps/dealership_api_starter/controllers/LeaseContractController.java
+++ src/main/java/com/ps/dealership_api_starter/controllers/LeaseContractController.java
@@ -1,41 +1,53 @@
-package com.ps.dealership_api_starter.controllers;
+ package com.ps.dealership_api_starter.controllers;
 
-import com.ps.dealership_api_starter.data.LeaseContractsDao;
+import com.ps.dealership_api_starter.data.LeaseContractDao;
 import com.ps.dealership_api_starter.models.LeaseContract;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-public class LeaseContractController implements LeaseContractsDao {
+public abstract class LeaseContractController implements LeaseContractDao {
 
     @RestController
     @RequestMapping("leasecontracts")
     @CrossOrigin
+
     public class LeaseContractsController {
 
         private LeaseContractDao leaseContractDao;
 
         @Autowired
-        public LeaseContractController(LeaseContractDao leaseContractDao) {
-            this.leaseContractDao = leaseContractDao;
+        public LeaseContractsController(LeaseContractDao leaseContractsDao) {
+            this.leaseContractDao = leaseContractsDao;
         }
 
         @GetMapping("{id}")
         public LeaseContract getLeaseContractById(@PathVariable Long id) {
             try {
-                return leaseContractDao.getLeaseContractById(id);
+
+                var leaseContract = leaseContractDao.getLeaseContractById(id);
+
+                if (leaseContract == null){
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+                }
+                return leaseContract;
 
             } catch (Exception e) {
-                e.printStackTrace();
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+
             }
         }
 
         @PostMapping("")
         public LeaseContract addLeaseContract(@RequestBody LeaseContract leaseContract) {
-            try {
+            try
+            {
                 return leaseContractDao.createLeaseContract(leaseContract);
             } catch (Exception e) {
-                e.printStackTrace();
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+
             }
         }
     }
